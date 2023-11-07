70ed30ddd38e0d2b5fc0d0c4e04b2f67c13078d5
Vicente <33096355+Vicente015@users.noreply.github.com>
2023-11-07 13:46:01 +0000

    Añadir selector categorías

--- crear_producto.php
+++ crear_producto.php
@@ -1,29 +1,35 @@
 <?php
 
+include "utils/database.php";
 include "utils/subida_archivos.php";
+include "utils/validacion.php";
 
 $mensajeError = "";
 $nombre = $_POST["nombre"];
 $precio = $_POST["precio"];
 $categoria = $_POST["categoria"];
 
+//* Cargar categorías
+$sql = "SELECT Id, Nombre FROM Categorías";
+$stmt = $conn->query($sql);
+$categorias = $stmt->fetchAll();
+// TODO: Catch de posible error aquí
+
 // TODO: Si hay algún error de validación, informará al usuario de cuáles son los errores y le permitirá, mediante un enlace, volver a rellenar el formulario
 // TODO: Si no hay ningún error, se mostrará un mensaje de confirmación y mediante un enlace se podrá acceder al menú principal.
 
+// ? Si hay POST validar
+if (!empty($_POST)) {
+  //* Validación
+  validarCampos($nombre, $precio, $categoria);
+  validarImagen();
 
-//* Validación
+  //? Si hay errores
+  if (count($errores) > 0) {
+  }
 
-// Muestra el mensaje de error
-if (!empty($mensajeError)) {
-  echo $mensajeError;
-} else {
   //* Subida archivos
   $subidaCorrecta = subir_archivos("ficheros/");
-  if (!$subidaCorrecta) {
-    echo "Error al subir los archivos.";
-  } else  {
-    echo "Formulario válido!!! :))";
-  }
 }
 
 ?>
@@ -31,41 +37,53 @@ if (!empty($mensajeError)) {
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Crear producto</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/light.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-  <style>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crear producto</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/light.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <style>
     span {
       color: var(--text-bright);
     }
-  </style>
-</head>
+    </style>
+  </head>
 
-<body>
-  <header>
-    <h1>CRUD</h1>
-    <a href="crear_producto.php"><span class="fa-solid fa-plus-circle"></span>Crear producto</a>
-    <a href="listado_productos.php"><span class="fa-solid fa-repeat"></span>Consultar listado</a>
-    <a href="edita_producto.php"><span class="fa-solid fa-pen-to-square"></span>Modificar producto</a>
-    <a href=""><span class="fa-solid fa-trash"></span>Eliminar producto</a>
-  </header>
-  <main>
-    <form action="crear_producto.php" method="post" enctype="multipart/form-data">
-      <label for="nombre">Nombre: </label>
-      <input type="text" name="nombre" id="nombre" required>
-      <label for="precio">Precio: </label>
-      <input type="number" name="precio" id="precio" min="0" required>
-      <label for="imagen">Imagen: </label>
-      <input type="file" name="imagen" id="imagen" required>
-      <label for="categoria">Categoría: </label>
-      <input type="number" name="categoria" id="categoria" min="0" required>
-      <button type="reset">Reset</button>
-      <button type="submit">Enviar</button>
-    </form>
-  </main>
-</body>
+  <body>
+    <header>
+      <h1>CRUD</h1>
+      <a href="crear_producto.php"><span class="fa-solid fa-plus-circle"></span>Crear producto</a>
+      <a href="listado_productos.php"><span class="fa-solid fa-repeat"></span>Consultar listado</a>
+      <a href="edita_producto.php"><span class="fa-solid fa-pen-to-square"></span>Modificar producto</a>
+      <a href=""><span class="fa-solid fa-trash"></span>Eliminar producto</a>
+    </header>
+    <main>
+      <?php if(empty($DATOS)): ?>
+      <form action="crear_producto.php" method="post" enctype="multipart/form-data">
+        <label for="nombre">Nombre: </label>
+        <input type="text" name="nombre" id="nombre" required>
+        <label for="precio">Precio: </label>
+        <input type="number" name="precio" id="precio" min="0" required>
+        <label for="imagen">Imagen: </label>
+        <input type="file" name="imagen" id="imagen" required>
+        <label for="categoria">Categoría: </label>
+        <select name="categori" id="categoria">
+          <?php
+        // Cargar categorias
+        foreach ($categorias as $categoria) {
+          echo "<option value='" . $categoria['Id'] . "'>" . $categoria['Nombre'] . "</option>";
+        }
+        ?>
+        </select>
+        <button type="reset">Reset</button>
+        <button type="submit">Enviar</button>
+      </form>
+      <?php elseif($mostrarConfirmacion === true): ?>
+      // TODO: Mensaje confirmación
+      <?php elseif($mostrarErrores === true): ?>
+      <?php endif; ?>
+    </main>
+  </body>
 
 </html>
--- utils/validacion.php
+++ utils/validacion.php
@@ -2,12 +2,12 @@
 
 $errores = [];
 
-function validarCampos($nombre, $precio, $categoria) {
+function validarCampos($nombre, $precio, $categoria)
+{
   if (empty($nombre) || empty($precio) || empty($categoria)) {
     array_push($errores, "Todos los campos son requeridos. Por favor, complete el formulario.");
   }
 
-  // TODO: Categoría obtenida de la DB
   // Validación del precio y la categoría como números mayores que 0
   if (!is_numeric($precio) || !is_numeric($categoria) || $precio <= 0 || $categoria <= 0) {
     array_push($errores, "El precio y la categoría deben ser números mayores que 0.");
@@ -16,7 +16,12 @@ function validarCampos($nombre, $precio, $categoria) {
 
 // TODO: Devolver errores
 
-function validarImagen() {
+function validarImagen()
+{
+  if (empty($_FILES)) {
+    array_push($errores, "No ha adjuntado ningún archivo.");
+    return;
+  }
   $imagen = $_FILES["imagen"];
   $imagenNombre = $imagen["name"];
   $imagenTipo = strtolower(pathinfo($imagenNombre, PATHINFO_EXTENSION));
@@ -24,6 +29,6 @@ function validarImagen() {
   // Verifica que el archivo sea una imagen
   $permitidos = array("jpg", "jpeg", "png", "pdf");
   if (!in_array($imagenTipo, $permitidos)) {
-    $mensajeError = "El archivo no es válido. Debe ser un archivo en formato JPG, JPEG, PNG, PDF.";
+    array_push($errores, "El archivo no es válido. Debe ser un archivo en formato JPG, JPEG, PNG, PDF.");
   }
 }
